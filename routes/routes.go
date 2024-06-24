80ca158d16222143b19fec47581a796188638079
miguel <miguel.gfe@outlook.com>
2024-06-24 14:21:26 -0300

    feat: adds CORS to project

--- routes/routes.go
+++ routes/routes.go
@@ -5,16 +5,19 @@ import (
 	"net/http"
 
 	"github.com/MiguelGFerreira/go-api-rest/controllers"
+	"github.com/MiguelGFerreira/go-api-rest/middleware"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/personalities", controllers.AllPersonalities).Methods("Get")
 	r.HandleFunc("/personalities/{id}", controllers.GetPersonalitie).Methods("Get")
 	r.HandleFunc("/personalities", controllers.CreatePersonalitie).Methods("Post")
 	r.HandleFunc("/personalities/{id}", controllers.DeletePersonalitie).Methods("Delete")
 	r.HandleFunc("/personalities/{id}", controllers.PutPersonalitie).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
